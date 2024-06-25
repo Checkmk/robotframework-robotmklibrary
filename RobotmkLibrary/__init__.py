4618b5592226ef0aef8d4bd84249260a790510cd
Joerg Herbel <joerg.herbel@checkmk.com>
2024-06-25 18:01:13 +0200

    Initial setup
    CMK-17596
    CMK-17597

---
+++ RobotmkLibrary/__init__.py
@@ -0,0 +1,8 @@
+# pylint: disable=invalid-name, missing-module-docstring
+
+
+def monitor_subsequent_keyword_runtime(  # pylint: disable=missing-function-docstring
+    *,
+    discover_as: str,  # pylint: disable=unused-argument
+) -> None:
+    return
