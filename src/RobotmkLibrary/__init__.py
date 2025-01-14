fa715137674e4524190806a096fa2a233c263c7a
Jörg Herbel <joerg.herbel@checkmk.com>
2025-01-14 14:56:37 +0100

    Enable all ruff rules + disable specific ones

--- src/RobotmkLibrary/__init__.py
+++ src/RobotmkLibrary/__init__.py
@@ -1,13 +1,14 @@
+from __future__ import annotations
+
 import logging
-from typing import Optional
 
 
 def monitor_subsequent_keyword_runtime(
     *,
-    discover_as: Optional[str] = None,
+    discover_as: str | None = None,
 ) -> None:
     logging.info(
         "The subsequent keyword will be discovered in Checkmk using its own name"
         if discover_as is None
-        else f'The subsequent keyword will be discovered in Checkmk as: "{discover_as}"'
+        else f'The subsequent keyword will be discovered in Checkmk as: "{discover_as}"',
     )
